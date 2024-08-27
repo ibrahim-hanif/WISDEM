fcb88c592465456f55736d5dab78bca176f0a652
ibrahim-hanif <guptavasudev37@gmail.com>
2024-08-27 10:45:25 +0200

    mpi bugfix issue560, Ex2 completed

--- examples/02_reference_turbines/iea15mw_driver.py
+++ examples/02_reference_turbines/iea15mw_driver.py
@@ -10,3 +10,25 @@ fname_analysis_options = mydir + os.sep + "analysis_options.yaml"
 
 wt_opt, analysis_options, opt_options = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
 
+#(v) OUTPUTS:
+"""
+==============
+comp.wt.wt_rna
+==============
+NL: NLBGS 1 ; 4.8908897e+11 1
+NL: NLBGS 2 ; 12033602 2.46041165e-05
+NL: NLBGS 3 ; 277408.174 5.67193682e-07
+NL: NLBGS 4 ; 6976.91684 1.42651282e-08
+NL: NLBGS 5 ; 175.207371 3.58232105e-10
+NL: NLBGS Converged
+RuntimeWarning: \\home.ansatt.ntnu.no\vasudevg\Vasudev_Gupta\WISDEM\wisdem\commonse\utilization_dnvgl.py:322
+The number of calls to function has reached maxfev = 50.ORBIT library intialized at '\\home.ansatt.ntnu.no\vasudevg\Vasudev_Gupta\WISDEM\wisdem\library'
+########################################
+Objectives
+Turbine AEP: 77.9037579237 GWh
+Blade Mass:  68206.4068005262 kg
+LCOE:        84.4284950361 USD/MWh
+Tip Defl.:   25.8318262264 m
+########################################
+Completed in, 34.94051551818848 seconds
+"""
--- examples/02_reference_turbines/nrel5mw_driver.py
+++ examples/02_reference_turbines/nrel5mw_driver.py
@@ -12,8 +12,37 @@ fname_analysis_options = mydir + os.sep + "analysis_options.yaml"
 wt_opt, analysis_options, opt_options = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
 # end
 
-print("blade mass:", wt_opt["wt.tcc.blade_mass"])
-print("blade moments of inertia:", wt_opt["wt.drivese.blades_I"])
-print("BRFM:", wt_opt["wt.drivese.pitch_system.BRFM"])
-print("hub forces:", wt_opt["wt.drivese.F_hub"])
-print("hub moments:", wt_opt["wt.drivese.M_hub"])
+#(v) fixed error: for below line(s)
+# - raise KeyError('{}: Variable "{}" not found.'.format(self.msginfo, name))
+# - KeyError: '<model> <class Group>: Variable "wt.tcc.blade_mass" not found.'
+# checked outputs/refturb_output.csv -> keys are not wt.tcc... but just tcc. (so changed below)
+print("blade mass:", wt_opt["tcc.blade_mass"])
+print("blade moments of inertia:", wt_opt["drivese.blades_I"])
+print("BRFM:", wt_opt["drivese.pitch_system.BRFM"])
+print("hub forces:", wt_opt["drivese.F_hub"])
+print("hub moments:", wt_opt["drivese.M_hub"])
+
+#(v) OUTPUTS:
+"""
+==============
+comp.wt.wt_rna
+==============
+NL: NLBGS 1 ; 4.06371192e+11 1
+NL: NLBGS 2 ; 4760440.29 1.17145122e-05
+NL: NLBGS 3 ; 0 0
+NL: NLBGS Converged
+######################################
+Objectives
+Turbine AEP: 23.9127972556 GWh
+Blade Mass:  16419.8666989212 kg
+LCOE:        51.0804383502 USD/MWh
+Tip Defl.:   4.7295143980 m
+########################################
+Completed in, 33.4929256439209 seconds
+blade mass: [16419.86669892]
+blade moments of inertia: [35590815.60501074 17795407.80250537 17795407.80250537        0.
+        0.                0.        ]
+BRFM: [-16256766.58649734]
+hub forces: [1171726.24681838    9709.60828864    9930.09366144]
+hub moments: [11075137.65779353  1317752.66701242  1200194.31654884]
+"""
