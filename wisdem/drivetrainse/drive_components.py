80cf77181201dc87c8641e2c37a795084952affa
ibrahim-hanif <guptavasudev37@gmail.com>
2026-01-05 12:55:12 +0100

    changed mb_e default to 3.5 and its description

--- wisdem/drivetrainse/drive_components.py
+++ wisdem/drivetrainse/drive_components.py
@@ -63,7 +63,7 @@ class MainBearing(om.ExplicitComponent):
         self.add_input("D_bearing", 0.0, units="m")
         self.add_input("D_shaft", 0.0, units="m")
         self.add_input("mb_mass_user", 0.0, units="kg")
-        self.add_input("mb_e", 4.0, desc="ISO 281: limiting value of Fa/Fr for the applicability of different values of factors X and Y") #(v)
+        self.add_input("mb_e", 3.5, desc="limiting value of Fa/Fr for the applicability of different values of factors X and Y (ISO 281)") #(v)
 
         self.add_output("mb_max_defl_ang", 0.0, units="rad")
         self.add_output("mb_mass", 0.0, units="kg")
