d6a7c895d1803dc6e3e9c8aa401435a1e6356a8f
ibrahim-hanif <guptavasudev37@gmail.com>
2026-03-02 16:15:25 +0100

    new connections for Analy_* with analy_MBforces_EBbeam model

--- wisdem/drivetrainse/drivetrain.py
+++ wisdem/drivetrainse/drivetrain.py
@@ -424,7 +424,7 @@ class DrivetrainSE_M4W( om.Group ):
                 promotes_outputs=["constr_L10_mb1","constr_L10_mb2"]
             )
             # -connecting = bear(1,2) -to- Analy_*
-            self.connect("bear2.bearing_type", "mb_fls.mb2_type")
+            # self.connect("bear2.bearing_type", "mb_fls.mb2_type") # define outside after prob setup, coz bearing_type is user input
             self.connect("bear2.mb_p", "mb_fls.p_mb") # same for both MBs ---
             self.connect("bear2.mb_X1", "mb_fls.X1_mb")
             self.connect("bear2.mb_Y1", "mb_fls.Y1_mb")
