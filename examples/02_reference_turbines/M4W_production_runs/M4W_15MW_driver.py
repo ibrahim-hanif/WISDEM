ecd5568921f5f70df488ec68db896aebf6c2fb24
ibrahim-hanif <guptavasudev37@gmail.com>
2026-03-23 13:54:03 +0100

    first m4w turbine driver working script, alhamdolillah

--- examples/02_reference_turbines/M4W_production_runs/M4W_15MW_driver.py
+++ examples/02_reference_turbines/M4W_production_runs/M4W_15MW_driver.py
@@ -3,8 +3,7 @@
 # purpose: copy of `iea15mw_driver.py` to test M4W modifications
 # 
 # ### current version:
-# NOTE: DD (direct-drive), monopile offshore
-# DT layout (without M4W GB optim):
+# mainly 'support structure' optimization with a (given) drivetrain/RNA (result of 03_)
 #
 # - objective: (1) `nacelle_mass` minimization (`NacelleSystemAdder`)
 #
@@ -20,69 +19,320 @@
 #%%
 import os
 from wisdem import run_wisdem
+import numpy as np
+import matplotlib.pyplot as plt
+from wisdem.commonse.utilities import load_all_mat_to_dict
+
+#%%
+wt_m4w = True # turbine to analyse: True = m4w / False = iea15mw
+flag_plot = True
+verbose = False
+flag_override_hub_loads = False # TODO: not working; make a flag in model_opts which removes connections
 
 #%%
 ## File management
 mydir = os.path.dirname(os.path.realpath(__file__))  # get path to this file
-fname_wt_input = mydir + os.sep + "M4W-15-240-WT.yaml"
+dir_02_ref_turbines = os.path.dirname(mydir)  # get path to 02_reference_turbines
+
+# ---- wind turbine geometry
+fname_wt_m4w = mydir + os.sep + "M4W-15-ieaMonopile-WT.yaml"
+fname_wt_iea15mw = dir_02_ref_turbines + os.sep + "IEA-15-240-RWT.yaml"
+if wt_m4w:
+      fname_wt_input = fname_wt_m4w
+else:
+     fname_wt_input = fname_wt_iea15mw
+
+# ---- modelling options
 fname_modeling_options = mydir + os.sep + "modeling_options.yaml"
-fname_analysis_options = mydir + os.sep + "analysis_options.yaml"
+# ---- analysis/optimization options
+fname_analysis_options = mydir + os.sep + "analysis_options_NOopt.yaml"
+
+#%% Loads from hub: overwrite values TODO: rotorse overwrites it at run
+if flag_override_hub_loads:
+      dir_loads = "M:\\Vasudev_Gupta\\outputs_mainshaft_loads"
+      loc_all_loads_mat_file = os.path.join(dir_loads, "mainshaft_loads.mat")
+      S_all,_ = load_all_mat_to_dict(loc_all_loads_mat_file)
+
+      F_aero_hub =np.array( [S_all['Fx_max'], S_all['Fy_max'], S_all['Fz_max']] ).reshape((3, 1))
+      M_aero_hub =np.array( [S_all['Mx_max'], S_all['My_max'], S_all['Mz_max']] ).reshape((3, 1))
+      
+      # can't override coz (required) rotorse overwrites them in run
+      overrides = {
+           'drivese.F_aero_hub': F_aero_hub, 'drivese.M_aero_hub': M_aero_hub
+      }
+else: overrides = None
+
 #%%
 wt_opt, analysis_options, opt_options = run_wisdem(
-    fname_wt_input, fname_modeling_options, fname_analysis_options
+    fname_wt_input, fname_modeling_options, fname_analysis_options,
+    overridden_values=overrides
 )
+# TODO
+# 1. overwrite hub loads from saved (ULS) file?
+# 2. check iea report for tower util plots... not mentioned?
+# 3. ! ~ full DT optimization takes toooo LOOOONG !
+# - do full DT optim using 03_
+# - restrict to some DT DVs? take vals from 03_
+# - check gradients wrt. each DV -- bad scaling?
+# %%[markdown]
+# # _____ Post-processing _____
+
+# %%
+doMBfls = analysis_options["flags"]["mb_fls"]
+# Print the results
+print("F_aero_hub:")
+print(" ", wt_opt["drivese.F_aero_hub"]/1e6, " MN" )
+print("M_aero_hub:")
+print(" ", wt_opt["drivese.M_aero_hub"]/1e6, " MNm \n" )
+
+# ---- 1P and 3P freq ranges
+rpm_min = wt_opt['drivese.minimum_rpm'][0]
+rpm_rated = wt_opt['drivese.rated_rpm'][0]
+freq_range_1P = np.array( [rpm_min, rpm_rated] )/60
+freq_range_3P = 3* freq_range_1P
+print("1P (blade period) freq ranges:")
+print(" ", freq_range_1P, " Hz" )
+print("3P (blade passing) freq ranges:")
+print(" ", freq_range_3P, " Hz \n" )
+
+print("LSS desvars:")
+print(" ", wt_opt["drivese.L_h1"], wt_opt["drivese.L_12"], wt_opt["drivese.lss_diameter"], wt_opt["drivese.lss_wall_thickness"] )
+# TODO: for flange mass, dohub (cf. var `flange_t2shell_t`)
+print("HSS desvars:")
+print(" ", wt_opt["drivese.L_hss"], wt_opt["drivese.hss_diameter"], wt_opt["drivese.hss_wall_thickness"] )
+print("Bedplate desvars (w_f, t_f, t_w):")
+print(" ", wt_opt["drivese.bedplate_flange_width"], wt_opt["drivese.bedplate_flange_thickness"], wt_opt["drivese.bedplate_web_thickness"] )
+print(" ")
+if doMBfls:
+    print("constr_L10_mb(1,2):", wt_opt["drivese.constr_L10_mb1"], wt_opt["drivese.constr_L10_mb2"] )
+print("--- constr_ max ---")
+print("- lss: ",
+      np.max(wt_opt["drivese.constr_lss_vonmises"])
+      )
+print("- hss: ",
+      np.max(wt_opt["drivese.constr_hss_vonmises"])
+      )
+print("- bedplate: ",
+      np.max(wt_opt["drivese.constr_bedplate_vonmises"])
+      )
+
+print("\nTower-top / drivetrain bedplate base loads:")
+print(" - base_F: ", wt_opt['drivese.base_F'])
+print(" - base_M: ", wt_opt['drivese.base_M'])
+#
+print("\n--- obj: masses ---")
+print(f"MSA mass: {wt_opt["drivese.msa_mass"]}")
+print(f"nacelle mass: {wt_opt["drivese.nacelle_mass"]}")
+print(f"nacelle cm: {wt_opt["drivese.nacelle_cm"]}")
+
+print("\n--- RNA properties ---")
+print(f"RNA mass: {wt_opt["drivese.rna_mass"]}")
+print(f"RNA cm: {wt_opt["drivese.rna_cm"]}")
+# -----------------------------------------------------------------------
+
+#%% plotting options
+# main colors
+from my_util_tools import util_funcs
+loc_clr_scheme_m4w = util_funcs.loc_clr_scheme_m4w
+clrs_m4w = util_funcs.read_color_scheme(loc_clr_scheme_m4w)
+
+#%%[markdown]
+# ### Tower utilizations
+#%%
+def get_tower_utilizations( wt_opt ):
+     zs = wt_opt["towerse.z_full"]
+     ds = wt_opt["towerse.outer_diameter_full"]
+     ts = wt_opt["towerse.t_full"]
+     mass = wt_opt["towerse.tower_mass"]
+     cg = wt_opt["towerse.tower_center_of_mass"]
+     constr_d_to_t = wt_opt["towerse.constr_d_to_t"]
+     constr_taper = wt_opt["towerse.constr_taper"]
+     wind = wt_opt["towerse.env.Uref"]
+     freq = wt_opt["towerse.tower.structural_frequencies"]
+     modes_FA = wt_opt["towerse.tower.fore_aft_modes"]
+     modes_SS = wt_opt["towerse.tower.side_side_modes"]
+     defl_top = wt_opt["towerse.tower.top_deflection"]
+     F_tower_base = wt_opt["towerse.tower.turbine_F"]
+     M_tower_base = wt_opt["towerse.tower.turbine_M"]
+     constr_stress = wt_opt["towerse.post.constr_stress"]
+     constr_buckle_GL = wt_opt["towerse.post.constr_global_buckling"]
+     constr_buckle_Sh = wt_opt["towerse.post.constr_shell_buckling"]
+     # return all as dict
+     return {
+           'zs': zs, 'ds': ds, 'ts': ts, 'mass': mass, 'cg': cg,
+           'constr_d_to_t': constr_d_to_t, 'constr_taper': constr_taper,
+           'wind': wind, 'freq': freq, 'modes_FA': modes_FA, 'modes_SS': modes_SS,
+           'defl_top': defl_top, 'F_tower_base': F_tower_base, 'M_tower_base': M_tower_base,
+           'constr_stress': constr_stress, 'constr_buckle_GL': constr_buckle_GL,
+           'constr_buckle_Sh': constr_buckle_Sh
+      }
+     
+
+def print_tower_utilizations( dict_tower_utils ):
+      # unpack dict
+      zs = dict_tower_utils['zs']
+      ds = dict_tower_utils['ds']
+      ts = dict_tower_utils['ts']
+      mass = dict_tower_utils['mass']
+      cg = dict_tower_utils['cg']
+      constr_d_to_t = dict_tower_utils['constr_d_to_t']
+      constr_taper = dict_tower_utils['constr_taper']
+      wind = dict_tower_utils['wind']
+      freq = dict_tower_utils['freq']
+      modes_FA = dict_tower_utils['modes_FA']
+      modes_SS = dict_tower_utils['modes_SS']
+      defl_top = dict_tower_utils['defl_top']
+      F_tower_base = dict_tower_utils['F_tower_base']
+      M_tower_base = dict_tower_utils['M_tower_base']
+      constr_stress = dict_tower_utils['constr_stress']
+      constr_buckle_GL = dict_tower_utils['constr_buckle_GL']
+      constr_buckle_Sh = dict_tower_utils['constr_buckle_Sh']
+
+      print("zs =", zs)
+      print("ds =", ds)
+      print("ts =", ts)
+      print("mass (kg) =", mass)
+      print("cg (m) =", cg)
+      print("d:t constraint =", constr_d_to_t)
+      print("taper ratio constraint =", constr_taper)
+
+      print("\nwind: ", wind)
+      print("freq (Hz) =", freq)
+      print("Fore-aft mode shapes =", modes_FA)
+      print("Side-side mode shapes =", modes_SS)
+      print("top_deflection (m) =", defl_top)
+      print("Tower base forces (N) =", F_tower_base)
+      print("Tower base moments (Nm) =", M_tower_base)
+      print("stress =", constr_stress)
+      print("GL buckling =", constr_buckle_GL)
+      print("Shell buckling =", constr_buckle_Sh)
 
 #%%
-# OUTPUT (NOTE: this is the Direct-Drive version of IEA-15-240-RWT)
-print(wt_opt['components']['nacelle']['drivetrain'])
+z = 0.5 * (wt_opt["towerse.z_full"][:-1] + wt_opt["towerse.z_full"][1:])
+dict_tower_utils = get_tower_utilizations(wt_opt)
+if verbose: print_tower_utilizations( dict_tower_utils)
 
-# =========
-# wt.wt_rna
-# =========
-# NL: NLBGS 1 ; 4.89104182e+11 1
-# NL: NLBGS 2 ; 12997708.9 2.65745201e-05
-# NL: NLBGS 3 ; 302612.475 6.187076e-07
-# NL: NLBGS 4 ; 7634.39772 1.56089398e-08
-# NL: NLBGS 5 ; 192.414917 3.93402723e-10
-# NL: NLBGS Converged
-# ################################################
-# Computation of costs of the main turbine components from TurbineCostSE
-# Blade cost              759.840 k USD       mass 68208.643 kg
-# Pitch system cost       1091.542 k USD       mass 49391.033 kg
-# Hub cost                83.499 k USD       mass 21409.881 kg
-# Spinner cost            32.606 k USD       mass 2937.439 kg
-# ------------------------------------------------
-# Rotor cost              3487.167 k USD       mass 278364.281 kg
+if flag_plot:
+    stress = wt_opt["towerse.post.constr_stress"]
+    shellBuckle = wt_opt["towerse.post.constr_shell_buckling"]
+    globalBuckle = wt_opt["towerse.post.constr_global_buckling"]
 
-# LSS cost                187.235 k USD       mass 15734.038 kg
-# Main bearing cost       138.535 k USD       mass 30785.643 kg
-# Gearbox cost            0.000 k USD       mass 0.000 kg #(v) TODO: GB vals = 0
-# HSS cost                0.000 k USD       mass 0.000 kg
-# Brake cost              88.018 k USD       mass 24278.014 kg
-# Generator cost          5003.335 k USD       mass 368839.407 kg
-# Bedplate cost           154.135 k USD       mass 53149.997 kg
-# Yaw system cost         233.956 k USD       mass 28187.494 kg
-# HVAC cost               1163.279 k USD       mass 9381.281 kg
-# Nacelle cover cost      117.160 k USD       mass 20554.306 kg
-# Electr connection cost  627.750 k USD
-# Controls cost           317.250 k USD
-# Other main frame cost   832.291 k USD
-# Transformer cost        575.938 k USD       mass 30635.000 kg
-# Converter cost          225.296 k USD       mass 11983.850 kg
-# ------------------------------------------------
-# Nacelle cost            9802.714 k USD       mass 672986.673 kg
+    plt.figure(figsize=(5.0, 3.5))
+    plt.subplot2grid((3, 3), (0, 0), colspan=2, rowspan=3)
+    plt.plot(stress, z,
+      label="stress", color=clrs_m4w['Aqua'])
+#     plt.plot(stress[:, 1], z, label="stress 2")
+    plt.plot(shellBuckle, z,
+      label="shell buckling", color=clrs_m4w['Red'])
+#     plt.plot(shellBuckle[:, 1], z, label="shell buckling 2")
+    plt.plot(globalBuckle, z,
+      label="global buckling", color=clrs_m4w['Dark_Green'])
+#     plt.plot(globalBuckle[:, 1], z, label="global buckling 2")
+    plt.axvline(1.0, color='k', linestyle='--', linewidth=1, label='1.0 limit')
+    plt.legend(bbox_to_anchor=(1.05, 1.0), loc=2)
+    plt.xlabel("utilization")
+    plt.ylabel("height along tower (m)")
+    plt.tight_layout()
+    plt.show()
 
-# Tower cost              2452.298 k USD       mass 853463.238 kg
-# ------------------------------------------------
-# ------------------------------------------------
-# Turbine cost            15742.178 k USD       mass 1804814.192 kg
-# Turbine cost per kW     1049.479 k USD/kW
-# ################################################
-# ORBIT library intialized at 'C:\Users\vasudevg\.conda\envs\wisdem-env\Lib\site-packages\library'
-# ########################################
-# Objectives
-# Turbine AEP: 77.9000397735 GWh
-# Blade Mass:  68208.6425948510 kg
-# LCOE:        75.3816041035 USD/MWh
-# Tip Defl.:   25.9398537736 m
-# ########################################
+#%%[markdown]
+# ### Monopile utilizations
+#%%
+def get_monopile_utilizations( wt_opt ):
+      zs = wt_opt["fixedse.z_full"]
+      ds = wt_opt["fixedse.outer_diameter_full"]
+      ts = wt_opt["fixedse.t_full"]
+      mass = wt_opt["fixedse.monopile_mass"]
+      cg = wt_opt["fixedse.monopile_z_cg"]
+      constr_d_to_t = wt_opt["fixedse.constr_d_to_t"]
+      constr_taper = wt_opt["fixedse.constr_taper"]
+      wind = wt_opt["fixedse.env.Uref"]
+      freq = wt_opt["fixedse.structural_frequencies"]
+      modes_FA = wt_opt["fixedse.fore_aft_modes"]
+      modes_SS = wt_opt["fixedse.side_side_modes"]
+      defl_top = wt_opt["fixedse.monopile.top_deflection"]
+      F_mudline = wt_opt["fixedse.monopile.mudline_F"]
+      M_mudline = wt_opt["fixedse.monopile.mudline_M"]
+      constr_stress = wt_opt["fixedse.post.constr_stress"]
+      constr_buckle_GL = wt_opt["fixedse.post.constr_global_buckling"]
+      constr_buckle_Sh = wt_opt["fixedse.post.constr_shell_buckling"]
+      # return all as dict
+      return {
+           'zs': zs, 'ds': ds, 'ts': ts, 'mass': mass, 'cg': cg,
+           'constr_d_to_t': constr_d_to_t, 'constr_taper': constr_taper,
+           'wind': wind, 'freq': freq, 'modes_FA': modes_FA, 'modes_SS': modes_SS,
+           'defl_top': defl_top, 'F_mudline': F_mudline, 'M_mudline': M_mudline,
+           'constr_stress': constr_stress, 'constr_buckle_GL': constr_buckle_GL,
+           'constr_buckle_Sh': constr_buckle_Sh
+      }
+
+def print_monopile_utilizations( dict_monopile_utils ):
+      # unpack dict
+      zs = dict_monopile_utils['zs']
+      ds = dict_monopile_utils['ds']
+      ts = dict_monopile_utils['ts']
+      mass = dict_monopile_utils['mass']
+      cg = dict_monopile_utils['cg']
+      constr_d_to_t = dict_monopile_utils['constr_d_to_t']
+      constr_taper = dict_monopile_utils['constr_taper']
+      wind = dict_monopile_utils['wind']
+      freq = dict_monopile_utils['freq']
+      modes_FA = dict_monopile_utils['modes_FA']
+      modes_SS = dict_monopile_utils['modes_SS']
+      defl_top = dict_monopile_utils['defl_top']
+      F_mudline = dict_monopile_utils['F_mudline']
+      M_mudline = dict_monopile_utils['M_mudline']
+      constr_stress = dict_monopile_utils['constr_stress']
+      constr_buckle_GL = dict_monopile_utils['constr_buckle_GL']
+      constr_buckle_Sh = dict_monopile_utils['constr_buckle_Sh']
+
+     # print results from the analysis or optimization
+      print("zs =", zs)
+      print("ds =", ds)
+      print("ts =", ts)
+      print("mass (kg) =", mass)
+      print("cg (m) =", cg)
+      print("d:t constraint =", constr_d_to_t)
+      print("taper ratio constraint =", constr_taper)
+
+      print("\nwind: ", wind )
+      print("freq (Hz) =", freq)
+      print("Fore-aft mode shapes =", modes_FA)
+      print("Side-side mode shapes =", modes_SS)
+      print("top_deflection (m) =", defl_top)
+      print("Tower base forces (N) =", F_mudline)
+      print("Tower base moments (Nm) =", M_mudline)
+      print("stress =", constr_stress)
+      print("GL buckling =", constr_buckle_GL)
+      print("Shell buckling =", constr_buckle_Sh)
+
+#%%
+z_monopile = 0.5 * (
+     wt_opt["fixedse.z_full"][:-1] + wt_opt["fixedse.z_full"][1:] )
+dict_monopile_utils = get_monopile_utilizations(wt_opt)
+if verbose: print_monopile_utilizations( dict_monopile_utils )
+
+if flag_plot:
+    stress = wt_opt["fixedse.post.constr_stress"]
+    shellBuckle = wt_opt["fixedse.post.constr_shell_buckling"]
+    globalBuckle = wt_opt["fixedse.post.constr_global_buckling"]
+
+    plt.figure(figsize=(5.0, 3.5))
+    plt.subplot2grid((3, 3), (0, 0), colspan=2, rowspan=3)
+    plt.plot(stress, z_monopile,
+      label="stress", color=clrs_m4w['Aqua'])
+#     plt.plot(stress[:, 1], z, label="stress 2")
+    plt.plot(shellBuckle, z_monopile,
+      label="shell buckling", color=clrs_m4w['Red'])
+#     plt.plot(shellBuckle[:, 1], z, label="shell buckling 2")
+    plt.plot(globalBuckle, z_monopile,
+      label="global buckling", color=clrs_m4w['Dark_Green'])
+#     plt.plot(globalBuckle[:, 1], z, label="global buckling 2")
+    plt.axvline(1.0, color='k', linestyle='--', linewidth=1, label='1.0 limit')
+    plt.legend(bbox_to_anchor=(1.05, 1.0), loc=2)
+    plt.xlabel("utilization")
+    plt.ylabel("height along monopile (m)")
+    plt.tight_layout()
+    plt.show()
+
+# %%
