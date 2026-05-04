05a96d57209aeb8e3f372adb06cc7f694ac331b3
ibrahim-hanif <guptavasudev37@gmail.com>
2026-05-04 17:22:37 +0200

    1. Hub_ not taking own loads as input; 2. collected pckgs import at top

--- examples/06_drivetrain/M4W_production_runs/00b_test_analyMB_pyFrame.py
+++ examples/06_drivetrain/M4W_production_runs/00b_test_analyMB_pyFrame.py
@@ -32,6 +32,7 @@ import wisdem.drivetrainse.drive_components as dc
 from wisdem.commonse.utilities import get_recorder_results, mainshaft_loads_from_mat_to_dict, load_all_mat_to_dict, pdf_norm_int_using_cdf, bin_counting_of_load, compute_LRD, compute_LRD_matrix_vectorized
 from wisdem.commonse.fileIO import var_df2dict
 import utilities_drivetrain as utilsDT
+from my_util_tools import analyseWTLoads, util_funcs, funcs_errors
 
 #%%
 # paths / locations
@@ -76,7 +77,6 @@ else: # define paths
 
 # %%
 # Define plotting options
-from my_util_tools import analyseWTLoads, util_funcs
 loc_clr_scheme_m4w = util_funcs.loc_clr_scheme_m4w
 clrs_m4w = util_funcs.read_color_scheme(loc_clr_scheme_m4w)
 
@@ -316,8 +316,9 @@ class test_Hub_Rotor_LSS_Frame( om.Group ):
         # Hub_Rotor_LSS_Frame:
         self.add_subsystem(
             "lss", ds.Hub_Rotor_LSS_Frame(
-                    n_dlcs=n_dlcs, modeling_options=opt_drivese,
-                    direct_drive=direct, openfast_options=opt_openfast
+                    n_dlcs=n_dlcs,
+                    modeling_options=opt_drivese,
+                    direct_drive=direct
                 ),
                 promotes=["*"]
             )
@@ -569,7 +570,6 @@ M_mb2_myframe[3,:] = np.hypot(M_mb2_myframe[1,:], M_mb2_myframe[2,:])
 #%%
 # plot options
 # --- main colors
-from my_util_tools import util_funcs
 loc_clr_scheme_m4w = util_funcs.loc_clr_scheme_m4w
 clrs_m4w = util_funcs.read_color_scheme(loc_clr_scheme_m4w)
 clr_Frame = clrs_m4w['Dark_Blue']
@@ -731,32 +731,23 @@ plt.show()
 # (https://towardsdatascience.com/time-series-forecast-error-metrics-you-should-know-cc88b8c67f27/)
 
 # NOTE: const diff
-def rmsError( vData, vRef ):
-    N = len(vRef)
-    return np.sqrt( np.sum((vData-vRef)**2)/N )
-
-def mapError( vData, vRef ):
-    "Mean Absolute Percentage Error"
-    N = len(vRef)
-    return np.sum( np.abs( (vData-vRef)*1e2 / vRef ) )/N
-
 # F_mb1_rad
 err_Fmb1_rad = F_mb1_beam[3,0,:] - F_mb1_myframe[3,:] # - 95600
-mape_Fmb1_rad = mapError( F_mb1_beam[3,0,:], F_mb1_myframe[3,:] )
+mape_Fmb1_rad = funcs_errors.mapError( F_mb1_beam[3,0,:], F_mb1_myframe[3,:] )
 print(
     f"F_mb1_rad | Error: max= {np.max( err_Fmb1_rad )}; map= {mape_Fmb1_rad}"
 )
 
 # F_mb2_ax
 err_Fmb2_ax = F_mb2_beam[0,0,:] - F_mb2_myframe[0,:] # = 78546 (due to gravity loads each ele)
-mape_Fmb2_ax = mapError( F_mb2_beam[0,0,:], F_mb2_myframe[0,:] )
+mape_Fmb2_ax = funcs_errors.mapError( F_mb2_beam[0,0,:], F_mb2_myframe[0,:] )
 print(
     f"F_mb2_ax | Error: max= {np.max( err_Fmb2_ax )}; map= {mape_Fmb2_ax}"
 )
 
 # F_mb2_rad
 err_Fmb2_rad = F_mb2_beam[3,0,:] - F_mb2_myframe[3,:] # = -1.0 * 1e6
-mape_Fmb2_rad = mapError( F_mb2_beam[3,0,:], F_mb2_myframe[3,:] )
+mape_Fmb2_rad = funcs_errors.mapError( F_mb2_beam[3,0,:], F_mb2_myframe[3,:] )
 print(
     f"F_mb2_rad | Error: max= {np.max( err_Fmb2_rad )}; map= {mape_Fmb2_rad}"
 )
@@ -764,7 +755,7 @@ print(
 # M_mb2_norm
 if mb2_Reactions[-1] > 0.0:
     err_Mmb2_rad = M_mb2_beam_norm - M_mb2_myframe[3,:]
-    mape_Mmb2_rad = mapError( M_mb2_beam_norm, M_mb2_myframe[3,:] )
+    mape_Mmb2_rad = funcs_errors.mapError( M_mb2_beam_norm, M_mb2_myframe[3,:] )
     print(
         f"M_mb2_rad | Error: max= {np.max( err_Mmb2_rad )}; map= {mape_Mmb2_rad}"
     )
