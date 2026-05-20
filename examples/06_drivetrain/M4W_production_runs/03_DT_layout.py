61929b7479d0beb1e9cee87ec8547bf35dbe138f
ibrahim-hanif <guptavasudev37@gmail.com>
2026-05-20 12:13:56 +0200

    results after 02_msa converg, AL

--- examples/06_drivetrain/M4W_production_runs/03_DT_layout.py
+++ examples/06_drivetrain/M4W_production_runs/03_DT_layout.py
@@ -78,14 +78,14 @@ record_cases = False    #TODO: add in final setup (full problem)
 # post-processing results
 plot_cases = True      #NOTE: saved, not changing now (commented)
 flag_scaling_show_browser = False
-flag_save_new_data = True
-load_from_saved_data = False
+flag_save_new_data = False
+load_from_saved_data = True
 
 # Parametric study
 flag_study_parametric = False
 # if True: init drive prob with 1 iter and reset to require (80) iters
 maxIter_param = 1
-maxIter_req = 5 * 16 # 80
+maxIter_req = 5 * 24 # 80 = 5*16
 if flag_study_parametric: maxIter = maxIter_param
 else: maxIter = maxIter_req
 
@@ -468,11 +468,11 @@ if doMBfls:
     prob["mb_fls.e_mb"] = prob["bear2.mb_e"]
 
 # Layout / lss inputs
-prob["L_h1"] = 0.31 #(def: 2.0), 4.25
-prob["L_12"] = 1.784 #(def:1.2), 7.1
+prob["L_h1"] = 0.3 #(def: 2.0), 0.310
+prob["L_12"] = 2.0 #(def:1.2), 1.7211
 prob["delta"] = 0.5
-prob["lss_diameter"] = np.array([3.205, 3.442]) #(def:1.0), 4.0
-prob["lss_wall_thickness"] = np.array([0.099, 0.071]) #(def:0.1), 0.3
+prob["lss_diameter"] = np.array([3.4, 3.2]) #(def:1.0), [3.440, 3.204]
+prob["lss_wall_thickness"] = np.array([0.09, 0.09]) #(def:0.1), [0.087, 0.082]
 
 # Gearbox inputs
 prob["gear_ratio"] = (375 / rated_rpm)
@@ -484,9 +484,9 @@ prob["gearbox_length_user"] = 2.512381653*1.1 # (from DOE_GBgen_updated.csv, +10
 prob["gearbox_radius_user"] = 2.10184*1.1 #(from DOE_GBgen_updated.csv, +10% margin)
 
 # HSS (DONE: consider as DV if needed)
-prob["L_hss"] = 1.102560612058607
-prob["hss_diameter"] = np.array([0.5, 0.5])
-prob["hss_wall_thickness"] = np.array([0.10124311401070424, 0.004])
+prob["L_hss"] = 0.5
+prob["hss_diameter"] = np.array([0.2, 0.2])
+prob["hss_wall_thickness"] = np.array([0.1, 0.01])
 
 # === Generator inputs (DONE: add compn later)
 # - needed by Bedplate_IBeam_Frame in drive_structure.py, output of HSS_Frame
@@ -557,9 +557,9 @@ prob["drive_height"] = 5.614 # (def: 5.614 for 15MW DD)
 
 # bedplate: Hub:_Rotor_LSS_Frame, Bedplate_IBeam_Frame inputs
 # --- below vals from ONLY bedplate optim (desvars, constr) for nacelle mass min
-prob["bedplate_flange_width"] = 2.3427547391232055 # 0.5
-prob["bedplate_flange_thickness"] = 0.09282021508604103 # 0.02
-prob["bedplate_web_thickness"] = 0.06152041352861306 #0.029
+prob["bedplate_flange_width"] = 1.0 # 0.5
+prob["bedplate_flange_thickness"] = 0.01 # 0.02
+prob["bedplate_web_thickness"] = 0.01 #0.029
 
 # `Hub_*` requires:
 prob["shaft_deflection_allowable"] = 1e-4 # within Hub_Rotor_LSS_Frame (below): Deflections and rotations at GB attachment
@@ -591,6 +591,7 @@ prob["material_names"] = ["steel", "steel_drive", "cast_iron", "glass_uni"]
 
 #%% overwrite variables from saved data
 if load_from_saved_data:
+    print(" loading prob vars from saved csv")
     prob = load_data( loc_save_data+".csv", prob )
 
 #%% load DOE case ( GR = 49 ) ?
@@ -644,8 +645,8 @@ print(" ", prob["L_h1"], prob["L_12"], prob["lss_diameter"], prob["lss_wall_thic
 # TODO: for flange mass, dohub (cf. var `flange_t2shell_t`)
 print("HSS desvars:")
 print(" ", prob["L_hss"], prob["hss_diameter"], prob["hss_wall_thickness"] )
-print("Bedplate desvars (w_f, t_f, t_w):")
-print(" ", prob["bedplate_flange_width"], prob["bedplate_flange_thickness"], prob["bedplate_web_thickness"] )
+print("Bedplate desvars (w_f, t_f, h_w, t_w):")
+print(" ", prob["bedplate_flange_width"], prob["bedplate_flange_thickness"], prob["bedplate_web_height"], prob["bedplate_web_thickness"] )
 print(" ")
 print("F_mb*:")
 print(" ", prob["F_mb1"], prob["F_mb2"] )
