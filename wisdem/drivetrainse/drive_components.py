e0a14d31846e0701dc013c99cb9b81a9da686833
ibrahim-hanif <guptavasudev37@gmail.com>
2026-07-27 16:28:38 +0200

    changed const bear's torsional stiff to linear depend on D and C

--- wisdem/drivetrainse/drive_components.py
+++ wisdem/drivetrainse/drive_components.py
@@ -1355,6 +1355,7 @@ class MainBearing_withDerivatives(om.ExplicitComponent):
     # width = a*D + b
     # mass  = k*D^n
     # Cr    = c*D^m
+    # k_tor = K*Cr*D = K*c*D^(m+1); K=457.502, from k_torsional=(3e10-6e8) for D_shaft=3.40678 for TRB2
     # ----
     # NOTE: `BEARINGS` dict not inside self, but stored at class/module level:
     # - coz static data (same for all components, independent of inputs/outputs)
@@ -1366,35 +1367,35 @@ class MainBearing_withDerivatives(om.ExplicitComponent):
         "CARB":
             dict(a=0.4299, b=0.0382, k=3682.8, n=2.7676, c=16676, m=1.4746,
                  max_ang=np.deg2rad(0.5), reactions=[0,1,1,0,0,0],
-                 k_torsional=0.0
+                 coeff_torsional=0.0
                  ),
 
         "CRB":
             dict(a=0.157,  b=0.0849, k=1070.8, n=1.8278, c=4526.5, m=0.9556,
                  max_ang=np.deg2rad(4/60), reactions=[0,1,1,0,0,0],
-                 k_torsional=0.0
+                 coeff_torsional=0.0
                  ),
 
         "SRB":
             dict(a=0.2463, b=0.185, k=2688.3,  n=1.8877, c=13878, m=1.0796,
                  max_ang=0.078, reactions=[1,1,1,0,0,0],
-                 k_torsional=0.0
+                 coeff_torsional=0.0
                  ),
 
         "TRB":
             dict(a=0.1499, b=0.0,    k=543.01, n=1.9043, c=1993.8, m=0.318,
                  max_ang=np.deg2rad(3/60), reactions=[1,1,1,0,1,1],
-                 k_torsional=3e10-6e8
+                 coeff_torsional=457.502
                  ),
 
         "TRB2":
             dict(a=0.1541, b=0.2087, k=1442.6, n=1.8932, c=6579.9, m=0.8592,
                  max_ang=np.deg2rad((0.06+0.02)/2), reactions=[1,1,1,0,1,1],
-                 k_torsional=3e10-6e8
+                 coeff_torsional=457.502
                  ),
     }
 
-    housing_factor = 1 + 80.0/27.0
+    housing_factor = 1 #+ 80.0/27.0
     kN_to_N = 1e3
 
     # ------------------------------------------------------------
@@ -1474,7 +1475,7 @@ class MainBearing_withDerivatives(om.ExplicitComponent):
         outputs["mb_X2"] = 0.67
         outputs["mb_Y2"] = outputs["mb_X2"]/np.tan(alpha)
         outputs["mb_p"] = 10/3
-        outputs["mb_k"] = data["k_torsional"] # torsional stiffness: k_yy=k_zz=k
+        outputs["mb_k"] = data["coeff_torsional"]*Cr*D_shaft # torsional stiffness: k_yy=k_zz=k
 
     # ------------------------------------------------------------
     # compute_partials (exact)
